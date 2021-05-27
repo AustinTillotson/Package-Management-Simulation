692b99696f5b30c97dde2478b9ff7628981a35eb
AustinTillotson <austintillotson@knights.ucf.edu>
2021-05-27 14:17:38 -0400

    Set up config input skeleton

--- src/Simulation.java
+++ src/Simulation.java
@@ -7,5 +7,23 @@
         Class: Simulation
  */
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Simulation {
+    public static void main(String[] args) {
+        int numStations = 0;
+        try {
+            File input = new File("config.txt");
+            Scanner scanInput = new Scanner(input);
+            numStations = scanInput.nextInt();
+            for(int i = 0; i < numStations; i++) {
+                System.out.printf("%d\n", scanInput.nextInt());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: Config file not found.\n");
+            e.printStackTrace();
+        }
+    }
 }
